370d6599c1736a94461af818a9364b3c70aa66d7
AKS <aks000203@gmail.com>
2020-01-24 02:48:09 +0530

    Friend-list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+
+  const {img, name, job} = props.person;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+      </div>
+    </div>)
 }
+
+const PersonList = () => {
+
+  const people = [
+    { img: 22, name: "ashu", job: "Dev" },
+    { img: 34, name: "abhi", job: "Student" },
+    { img: 56, name: "saurabh", job: "Dev" }
+  ]
+
+  return (
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]} />
+      <Person person={people[2]} />
+    </section>
+  )
+}
+
+const App = () => (<PersonList />)
 
 export default App;
